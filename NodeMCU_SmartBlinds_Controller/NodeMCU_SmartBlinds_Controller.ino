bfec48886ad750aba8a565259197c0173db7bb61
Alexander Magnusson <alexander.emanuel.magnusson@gmail.com>
2026-01-04 19:44:32 +0100

    Add non-blocking logic to run a servo smoothly (possibility to set speed) to control the blinds and detach after command execution to stop the buzzing.

--- NodeMCU_SmartBlinds_Controller/NodeMCU_SmartBlinds_Controller.ino
+++ NodeMCU_SmartBlinds_Controller/NodeMCU_SmartBlinds_Controller.ino
@@ -1,28 +1,47 @@
-const int LDR_SENSOR_PIN = A0;
+#include <Servo.h>
+
+
+const int LDR_SENSOR_PIN = A0; // NodeMCU analog input A0 (0-3.3V with internal voltage divider, scaled to 0-1023) , raw ESP8266 chip only accepts 0-1V on A0 !!!
+const uint8_t SERVO_PIN_BLINDS_1 = D5; // GPIO14 (D5) on NodeMCU
+
+Servo servoBlinds1;
 
 const unsigned long LDR_SENSOR_INTERVAL = 500;
 const float LDR_IIR_FILTER_ALPHA = 0.15f;
 
-const uint16_t GLARE_THRESHOLD = 165;
-const uint16_t GLARE_HYSTERESIS = 20;
+const uint16_t GLARE_THRESHOLD = 750;
+const uint16_t GLARE_HYSTERESIS = 50;
 const uint8_t TILT_OPEN = 90;
 const uint8_t TILT_GLARE = 30;
 
 // LDR-Sensor (PhotoResistor)
 uint16_t filtered_lightLevel_int;
 
-// command for servo
+// Servo state
+bool isServoBlinds1Attached = false;
+uint8_t currentPos = TILT_OPEN;
 uint8_t targetPos = TILT_OPEN;
-uint8_t lastTargetPos = TILT_OPEN;
+
+// Timing
+const unsigned long idleDetachTime = 1000; // miliseconds
+
+// Movement step
+const unsigned long moveInterval = 10; // ms between steps
 
 // constrains for servo movement
 const uint8_t SERVO_MIN = 0;
 const uint8_t SERVO_MAX = 180;
 
+
 void setup() {
   Serial.begin(115200);
 
   pinMode(LDR_SENSOR_PIN, INPUT);
+
+  // Servo 1 Init
+  servoBlinds1.attach(SERVO_PIN_BLINDS_1); 
+  isServoBlinds1Attached = true;
+  servoBlinds1.write(TILT_OPEN);
 }
 
 void loop() {
@@ -30,6 +49,7 @@ void loop() {
 
   readSensors(now);
   checkGlareProtection(filtered_lightLevel_int);
+  moveBlinds(now);
 }
 
 
@@ -48,6 +68,7 @@ void readSensors(unsigned long now){
 
 void checkGlareProtection(uint16_t lightLevel){
   static bool glareActive = false;
+  static uint8_t lastTargetPos = TILT_OPEN;
 
   if (!glareActive && lightLevel > GLARE_THRESHOLD + GLARE_HYSTERESIS) {
     glareActive = true;
@@ -57,7 +78,6 @@ void checkGlareProtection(uint16_t lightLevel){
   }
 
   targetPos = glareActive ? TILT_GLARE : TILT_OPEN;
-  targetPos = constrain(targetPos, SERVO_MIN, SERVO_MAX); // ensure the output is within valid range
 
   // for testing the servo command in a console
   if(targetPos != lastTargetPos){ // event-based logging
@@ -69,4 +89,35 @@ void checkGlareProtection(uint16_t lightLevel){
 
     lastTargetPos = targetPos;
   }
+}
+
+// Function to move blinds slowly to a desired target position
+void moveBlinds(unsigned long now){ 
+  static unsigned long lastStepTime_servoBlinds1 = 0;
+  static unsigned long lastMoveTime_servoBlinds1 = 0;
+
+  // Servo controling Blinds 1
+  if (currentPos != targetPos && now - lastStepTime_servoBlinds1 >= moveInterval) {
+
+    lastStepTime_servoBlinds1 = now;
+
+    if (!isServoBlinds1Attached) {
+      servoBlinds1.attach(SERVO_PIN_BLINDS_1); 
+      isServoBlinds1Attached = true;
+    }
+
+    // Step toward target
+    if (currentPos < targetPos) currentPos += 1;
+    if (currentPos > targetPos) currentPos -= 1;
+
+    servoBlinds1.write(currentPos);
+    lastMoveTime_servoBlinds1 = now; // reset idle timer
+  }
+
+  // Detach if idle for > 1s (to stop servo from buzzing when stationary)
+  if (isServoBlinds1Attached && now - lastMoveTime_servoBlinds1 > idleDetachTime) {
+    servoBlinds1.detach();
+    isServoBlinds1Attached = false;
+  }
+
 }
