52a7b4b83442e040382458009c1b9609018c8077
Alexander Magnusson <alexander.emanuel.magnusson@gmail.com>
2026-01-05 17:30:09 +0100

    Initial commit

---
+++ IoT_Smart_Blinds_Springboot_WebApp/src/main/java/org/xmagnusson/iot/smart/blinds/IoT_SmartBlindsWebApp.java
@@ -0,0 +1,12 @@
+package org.xmagnusson.iot.smart.blinds;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoT_SmartBlindsWebApp {
+    public static void main(String[] args) {
+        SpringApplication.run(IoT_SmartBlindsWebApp.class, args);
+    }
+}
